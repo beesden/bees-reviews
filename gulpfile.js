f3468ec6a31e2d8a49ab5588304757adf8c8bf8a
toby.marson <toby.marson@wearefriday.com>
2017-01-12 11:10:56 +0000

    Sass and menu / header redesign

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,17 @@ var gutil = require('gulp-util');
 var ngTemplates = require('gulp-angular-templatecache');
 
 var config = {
-    dist: 'dist',
-    script: "src/app/**/*.js",
-    style: "src/app.css",
-    template: "src/templates/**/*.html"
+	dist: 'dist',
+	script: "src/app/**/*.js",
+	style: "src/style/**/*.scss",
+	template: "src/templates/**/*.html"
 };
 
 var catchError = function(error, graceful) {
-    gutil.log(gutil.colors.magenta(stderr));
-    if (graceful) {
-        this.emit('end');
-    }
+	gutil.log(gutil.colors.magenta(stderr));
+	if (graceful) {
+		this.emit('end');
+	}
 };
 
 /**
@@ -29,7 +29,7 @@ var catchError = function(error, graceful) {
  * Delete previously built content
  */
 gulp.task('clean', function() {
-    return del(config.dist);
+	return del(config.dist);
 });
 
 /**
@@ -38,15 +38,15 @@ gulp.task('clean', function() {
  * Install all bower dependencies
  */
 gulp.task('bower', function(cb) {
-    var cmd = 'bower install -p';
-    var exec = require('child_process').exec;
+	var cmd = 'bower install -p';
+	var exec = require('child_process').exec;
 
-    return exec(cmd, function(err, stdout, stderr) {
-        if (stderr) {
-            catchError(error);
-        }
-        cb(err);
-    });
+	return exec(cmd, function(err, stdout, stderr) {
+		if (stderr) {
+			catchError(error);
+		}
+		cb(err);
+	});
 });
 
 /**
@@ -55,11 +55,11 @@ gulp.task('bower', function(cb) {
  * Build the scripts
  */
 gulp.task('scripts', function() {
-    return gulp.src(config.script)
-        .pipe(sourcemaps.init())
-        .pipe(concat('app.js'))
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest(config.dist));
+	return gulp.src(config.script)
+		.pipe(sourcemaps.init())
+		.pipe(concat('app.js'))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest(config.dist));
 });
 
 /**
@@ -68,8 +68,11 @@ gulp.task('scripts', function() {
  * Build the styles
  */
 gulp.task('styles', function() {
-    return gulp.src(config.style)
-        .pipe(gulp.dest(config.dist));
+	return gulp.src(config.style)
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest(config.dist));
 });
 
 /**
@@ -78,16 +81,16 @@ gulp.task('styles', function() {
  * Build the templates for the BB widget
  */
 gulp.task('templates', function() {
-    return gulp.src(config.template)
-        .pipe(ngTemplates({
-            module: 'app',
-            moduleSystem: 'IIFE',
-            //  standalone: true,
-            transformUrl: function(url) {
-                return '/templates/' + url;
-            }
-        }).on('error', catchError))
-        .pipe(gulp.dest(config.dist));
+	return gulp.src(config.template)
+		.pipe(ngTemplates({
+			module: 'app',
+			moduleSystem: 'IIFE',
+			//  standalone: true,
+			transformUrl: function(url) {
+				return '/templates/' + url;
+			}
+		}).on('error', catchError))
+		.pipe(gulp.dest(config.dist));
 });
 
 
@@ -97,10 +100,10 @@ gulp.task('templates', function() {
  * Watch for changes
  */
 gulp.task('watch', function() {
-    var watchConfig = {interval: 0.8};
-    gulp.watch(config.script, watchConfig, ['scripts']);
-    gulp.watch(config.style, watchConfig, ['styles']);
-    gulp.watch(config.template, watchConfig, ['templates']);
+	var watchConfig = {interval: 0.8};
+	gulp.watch(config.script, watchConfig, ['scripts']);
+	gulp.watch(config.style, watchConfig, ['styles']);
+	gulp.watch(config.template, watchConfig, ['templates']);
 });
 
 /**
@@ -109,9 +112,9 @@ gulp.task('watch', function() {
  * Complete build the dist BB widget
  */
 gulp.task('default', ['clean'], function(cb) {
-    return sequence.apply(this, [
-        'bower',
-        ['scripts', 'styles', 'templates'],
-        cb
-    ]);
+	return sequence.apply(this, [
+		'bower',
+		['scripts', 'styles', 'templates'],
+		cb
+	]);
 });
--- src/app/directives/menu.drv.js
+++ src/app/directives/menu.drv.js
@@ -2,61 +2,63 @@
 
 (function(ng) {
 
-    /**
-     * @ngdoc directive
-     * @name app:mainMenu
-     *
-     * @description - Display all the categories in the menu.
-     */
-    ng.module('app').directive('appMainNav', function(reviewDataService) {
+	/**
+	 * @ngdoc directive
+	 * @name app:mainMenu
+	 *
+	 * @description - Display all the categories in the menu.
+	 */
+	ng.module('app').directive('appMainNav', function($route, reviewDataService) {
 
-        return {
+		return {
 
-            restrict: 'A',
-            templateUrl: '/templates/_header.html',
+			restrict: 'A',
+			templateUrl: '/templates/_menu.html',
 
-            link: function(scope) {
+			link: function(scope) {
 
-                // Add the category 'hierarchy'
-                reviewDataService.getCategoryList().then(function(response) {
+				// Add the category 'hierarchy'
+				reviewDataService.getCategoryList().then(function(response) {
 
-                    scope.categories = [];
-                    var categoryMap = {};
+					scope.categories = [];
+					var categoryMap = {};
 
-                    // Create map of categories
-                    response.Results.forEach(function(category) {
-                        categoryMap[category.Id] = ng.merge({}, category, {children: []});
-                    });
+					// Create map of categories
+					response.Results.forEach(function(category) {
+						categoryMap[category.Id] = ng.merge({}, category, {children: []});
+					});
 
-                    // Add to hierarchy
-                    response.Results.forEach(function(category) {
-                        if (category.ParentId) {
-                            categoryMap[category.ParentId].children.push(category);
-                        } else {
-                            scope.categories.push(categoryMap[category.Id]);
-                        }
-                    });
+					// Add to hierarchy
+					response.Results.forEach(function(category) {
+						if (category.ParentId) {
+							categoryMap[category.ParentId].children.push(category);
+						} else {
+							scope.categories.push(categoryMap[category.Id]);
+						}
+					});
 
-                });
+				});
 
-                /**
-                 * Select a category to view it's children
-                 *
-                 * @param category
-                 */
-                scope.selectCategory = function(category) {
-                    if (scope.currentCategory === category.Id) {
-                        delete scope.currentCategory;
-                    } else {
-                        scope.currentCategory = category.Id;
-                    }
-                    return false;
-                };
+				/**
+				 * Select a category to view it's children
+				 *
+				 * @param category
+				 */
+				scope.selectCategory = function($event, category) {
+					if (scope.currentCategory === category.Id) {
+						delete scope.currentCategory;
+					} else {
+						scope.currentCategory = category.Id;
+					}
+					if (category.children.length) {
+						$event.preventDefault();
+					}
+				};
 
-            }
+			}
 
-        }
+		}
 
-    });
+	});
 
 })(angular);
