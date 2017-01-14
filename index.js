5d0e1696ef9dc831963db9282f6a9000e4b5c3a3
beesden <beesden@gmail.com>
2017-01-14 16:20:46 +0000

    Add workaround for browsers that don't support grid

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var path = require('path');
 
 // Start server
-    var app = express();
-	var port = process.env.PORT || 80;
+var app = express();
+var port = process.env.PORT || 80;
 
-    app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-    app.get('/', function(req, res) {
-        res.redirect('/reviews');
-    });
+app.get('/', function (req, res) {
+    res.redirect('/reviews');
+});
 
-    app.get(['/reviews', '/reviews/*'], function(req, res) {
-        res.sendFile(path.join(__dirname + '/index.html'));
-    });
+app.get(['/reviews', '/reviews/*'], function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-    app.listen(port, function() {
-        console.log('Example app listening on port ' + port + '!');
-    });
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
